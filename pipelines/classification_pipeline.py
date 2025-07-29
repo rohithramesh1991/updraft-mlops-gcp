f3f3a2916b7426263f8a54bd78cdc4ca91067be7
Roi <rohithramesh1991@gmail.com>
2025-07-29 11:35:20 +0100

    deploy_endpoint_4

--- pipelines/classification_pipeline.py
+++ pipelines/classification_pipeline.py
@@ -31,22 +31,13 @@ def classification_pipeline(project: str, dataset: str, table: str):
                     x_test_path=p.outputs["x_test_path"],
                     y_test_path=p.outputs["y_test_path"])
 
-    model_importer1 = importer(
-        artifact_uri=t.outputs["model_path"],
-        artifact_class=artifact_types.UnmanagedContainerModel,
-        metadata={
-            "containerSpec": {
-                "imageUri": "us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-0:latest"
-            }
-        }
-    )
-
     uploaded = ModelUploadOp(
         project=project,
         location="us-central1",
         display_name="classification-xgb-model",
-        unmanaged_container_model=model_importer1.outputs["artifact"]
+        unmanaged_container_model=t.outputs["model_path"]
     )
+
 
     deploy_op = ModelDeployOp(
         model=uploaded.outputs["model"],
