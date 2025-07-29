762488511f16356a28cea6b81a66f17af67dc9b4
Roi <rohithramesh1991@gmail.com>
2025-07-29 11:21:53 +0100

    deploy_endpoint_4

--- pipelines/classification_pipeline.py
+++ pipelines/classification_pipeline.py
@@ -31,7 +31,7 @@ def classification_pipeline(project: str, dataset: str, table: str):
                     x_test_path=p.outputs["x_test_path"],
                     y_test_path=p.outputs["y_test_path"])
 
-    model_importer = importer(
+    model_importer1 = importer(
         artifact_uri=t.outputs["model_path"],
         artifact_class=artifact_types.UnmanagedContainerModel,
         metadata={
@@ -45,7 +45,7 @@ def classification_pipeline(project: str, dataset: str, table: str):
         project=project,
         location="us-central1",
         display_name="classification-xgb-model",
-        unmanaged_container_model=model_importer.outputs["artifact"]
+        unmanaged_container_model=model_importer1.outputs["artifact"]
     )
 
     deploy_op = ModelDeployOp(
